ebd24112d53e968337704f8f0bf9d3b6156bf0c2
Aflaungos <vimavick@gmail.com>
2026-06-03 23:27:20 -0300

    a52xq: Fix namespaces
    Change-Id: I30ad75e243cadab878b9783479059b9dbaf6cf7f

--- extract-files.py
+++ extract-files.py
@@ -18,14 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/samsung/a52xq',
-    'hardware/qcom-caf/common/libqti-perfd-client',
-    'hardware/qcom-caf/sm8250',
-    'hardware/qcom-caf/wlan',
-    'hardware/samsung',
-    'vendor/qcom/opensource/commonsys/display',
-    'vendor/qcom/opensource/commonsys-intf/display',
-    'vendor/qcom/opensource/dataservices',
+    'vendor/samsung/sm7225-common',
     'vendor/qcom/opensource/display',
 ]
 
